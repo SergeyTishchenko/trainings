911fb4b498f8d4e419e6b6ad591db72c8815ba3e
SergeyTishchenko <sergey_tishchenko@tut.by>
2016-11-08 20:15:14 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,11 +1,11 @@
 def sum_of_numbers_is_7():
     result = []
-    for _ in range(100, 1000):
+    for i in range(100, 1000):
         summ = 0
-        for n in str(_):
+        for n in str(i):
             summ += int(n)
         if summ == 7:
-            result.append(_)
+            result.append(i)
     return result
 
 
--- 4.py
+++ 4.py
@@ -1,9 +1,9 @@
 def delete_spases(without_spases):
     result = []
     lst = without_spases.split(" ")
-    for _ in range(len(lst) - 1):
-        if len(lst[_]) != 0:
-            result.append(lst[_])
+    for i in range(len(lst) - 1):
+        if len(lst[i]) != 0:
+            result.append(lst[i])
     return " ".join(result)
 
 if __name__ == '__main__':

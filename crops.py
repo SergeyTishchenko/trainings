754425da0c7f97fbdf912a353baca05a4d77e357
SergeyTishchenko <sergey_tishchenko@tut.by>
2016-11-30 14:47:10 +0300

    Update crops.py

--- crops.py
+++ crops.py
@@ -4,36 +4,36 @@ class Crops(object):
     def __init__(self, growth_rate, light_need, water_need):
         # set attribute to the initial value
 
-        self._growth = 0
-        self._days_growing = 0
-        self._growth_rate = growth_rate
-        self._light_need = light_need
-        self._water_need = water_need
-        self._status = "Seed"
-        self._type = "Generic"
+        self.growth = 0
+        self.days_growing = 0
+        self.growth_rate = growth_rate
+        self.light_need = light_need
+        self.water_need = water_need
+        self.status = "Seed"
+        self.type = "Generic"
 
     def needs(self):
-        return {'light need': self._light_need, 'water need': self._water_need}  # Return dict of 'needs'
+        return {'light need': self.light_need, 'water need': self.water_need}  # Return dict of 'needs'
 
     def report(self):
-        return {'type': self._type, 'status': self._status, 'growth': self._growth, 'days growing': self._days_growing}
+        return {'type': self.type, 'status': self.status, 'growth': self.growth, 'days growing': self.days_growing}
 
     def update_status(self):
-        if self._growth > 15:
-            self._status = "Old"
-        elif self._growth > 10:
-            self._status = "Mature"
-        elif self._growth > 5:
-            self._status = "Young"
-        elif self._growth > 0:
-            self._status = "Seedling"
-        elif self._growth == 0:
-            self._status = "Seed"
+        if self.growth > 15:
+            self.status = "Old"
+        elif self.growth > 10:
+            self.status = "Mature"
+        elif self.growth > 5:
+            self.status = "Young"
+        elif self.growth > 0:
+            self.status = "Seedling"
+        elif self.growth == 0:
+            self.status = "Seed"
 
     def grow(self, light, water):
-        if light >= self._light_need and water >= self._water_need:
-            self._growth += self._growth_rate
-            self._days_growing += 1
+        if light >= self.light_need and water >= self.water_need:
+            self.growth += self.growth_rate
+            self.days_growing += 1
             self.update_status()
 
 

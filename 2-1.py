fd2fa8d034c6859aa903beac194cb745d3204c94
SergeyTishchenko <sergey_tishchenko@tut.by>
2016-12-08 11:41:15 +0300

    Update 2-1.py

--- 2-1.py
+++ 2-1.py
@@ -1,8 +1,8 @@
 def divider(n):
     i = 2
     j = 0
-    while i**2 <= n and j != 1:
-        if n%i == 0:
+    while i ** 2 <= n and j != 1:
+        if n % i == 0:
             j = 1
         i += 1
     if j == 1:
